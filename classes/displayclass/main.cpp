37ce298b4786857cdbb46dcf0cd5ac255eccb859
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-10-30 11:45:54 +0100

    Delete main.cpp

--- classes/displayclass/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "hwlib.hpp"
-#include "rtos.hpp"
-#include "display.hpp"
-
-int main( void ){	
-    auto displayClass = DisplayClass();
-	auto displayTask = DisplayTask(displayClass);
-	rtos::run();
-}
