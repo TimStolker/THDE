720972f2a20273307c8eef4fec1ca4217d465d1d
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-11-02 14:37:39 +0100

    Update main.cpp

--- classes/Final/main.cpp
+++ classes/Final/main.cpp
@@ -6,17 +6,20 @@
 #include "Registergame.hpp"
 #include "KeyPad.hpp"
 #include "receive.hpp"
+#include "TransferHitControl.hpp"
 
 int main( void ){	
     hwlib::wait_ms(500);
+    auto transfer = TransferHitControl();
     auto displayClass = DisplayClass();
     auto display = DisplayTask(displayClass);
     
     auto irSend = irSendControlClass();
     auto Init = InitGameControl(display,irSend);
-    auto runGame = RunGameClass(irSend, display, 60000 * rtos::ms);
+    auto runGame = RunGameClass(irSend, display, 60000 * rtos::ms, transfer);
     auto reg = Registergame(display, runGame);
     auto mykeypad = Keypadclass(Init,reg);
 
 	rtos::run();
 }
+
