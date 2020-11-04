2fac999ee49f5e9528b3d72bdc8cd95f45837741
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-11-04 20:41:40 +0100

    Create main.cpp

---
+++ PlayerFiles/main.cpp
@@ -0,0 +1,24 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+#include "display.hpp"
+#include "InitGameControl.hpp"
+#include "RunGameControl.hpp"
+#include "Registergame.hpp"
+#include "KeyPad.hpp"
+#include "receive.hpp"
+#include "TransferHitControl.hpp"
+
+int main( void ){	
+    hwlib::wait_ms(500);
+    auto transfer = TransferHitControl();
+    auto displayClass = DisplayClass();
+    auto display = DisplayTask(displayClass);
+    auto irSend = irSendControlClass();
+    auto Init = InitGameControl(display,irSend);
+    auto runGame = RunGameClass(irSend, display, 800 * rtos::ms, transfer);
+    auto reg = Registergame(display, runGame);
+    auto mykeypad = Keypadclass(Init,reg,runGame);
+     auto receive = irReceiveControlClass(runGame,reg);
+
+	rtos::run();
+}
