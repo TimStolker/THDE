31f12bac20db7d6abb5a41f36b1fc5201565e083
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-11-04 21:45:59 +0100

    Update receive.hpp

--- PlayerFiles/receive.hpp
+++ PlayerFiles/receive.hpp
@@ -1,12 +1,16 @@
+//this file contains Doxygen lines
+///file receive.hpp
+/// \brief receive class
+/// \details Contains all the necessary information about the receive class
+
 #include "hwlib.hpp"
 #include "rtos.hpp"
 #include "RunGameControl.hpp"
 #ifndef RECEIVE_HPP
 #define RECEIVE_HPP
-
 namespace target = hwlib::target; 
-
 class irReceiveControlClass : public rtos::task <>{
+
 private:
     uint16_t byte = 0x00;
     uint16_t byte_index = 32'768; //eerste bit links
@@ -32,7 +36,7 @@ private:
                     }
                     break;
                 }
-                 
+                    
                 case RECEIVE: {
                     byte = 0;
                     for(int i=0;i<16;i++){
@@ -100,15 +104,16 @@ private:
                     state = IDLE;
                     break;
                 }
+
             }
         }
     }
 
 public:
     irReceiveControlClass(RunGameClass & runGame, Registergame & regGame): 
-        rtos::task<>("irreceive"), 
-        runGame(runGame), 
-        regGame(regGame)
+    rtos::task<>("irreceive"), 
+    runGame(runGame), 
+    regGame(regGame)
     {}
 
 };
