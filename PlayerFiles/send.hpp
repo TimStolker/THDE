b5157cb40163158b19a2117a329c6c1768160670
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-11-04 20:41:57 +0100

    Create send.hpp

---
+++ PlayerFiles/send.hpp
@@ -0,0 +1,67 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+#ifndef SEND_HPP
+#define SEND_HPP
+namespace target = hwlib::target; 
+class irSendControlClass : public rtos::task <>{
+
+private:
+    rtos::flag flagShoot;
+    uint16_t Data;
+
+    void main(){
+        
+        uint16_t data_index = 32768; //meest linker bit
+        enum state_t {IDLE, SHOOT}; 
+        state_t state = IDLE;
+        auto ir = hwlib::target::d2_36kHz(); 
+        for(;;){
+            switch(state){
+                case IDLE: {
+                    auto evt = wait(flagShoot);
+                    if(evt == flagShoot){
+                        state = SHOOT;
+                    }
+                    break;
+                }
+                    
+                case SHOOT: {
+                    for(unsigned int i=0;i<16;i++){
+                        bool bit = (Data & (data_index >> i));
+                        if(bit){ 
+                            ir.write(1);
+                            ir.flush();
+                            hwlib::wait_us(1600);
+                            ir.write(0);
+                            ir.flush();
+                            hwlib::wait_us(800);
+                        }
+                        else{
+                            ir.write(1);
+                            ir.flush();
+                            hwlib::wait_us(800);
+                            ir.write(0);
+                            ir.flush();
+                            hwlib::wait_us(1600);
+                            
+                        }
+                    }
+                    state = IDLE;
+                break;
+                }
+            }
+        }
+    }
+
+public:
+    irSendControlClass():
+    rtos::task<>("irsend"), 
+    flagShoot(this, "flagShoot")
+    {}
+    
+    void setSignal(uint16_t data){ Data = data; flagShoot.set();}
+
+};
+
+#endif
