7c2e568a48b937bc47a82f28b5f76ab58c0e8bae
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2020-11-04 22:01:02 +0100

    Update receive.hpp

--- PlayerFiles/receive.hpp
+++ PlayerFiles/receive.hpp
@@ -8,9 +8,10 @@
 #include "RunGameControl.hpp"
 #ifndef RECEIVE_HPP
 #define RECEIVE_HPP
-namespace target = hwlib::target; 
-class irReceiveControlClass : public rtos::task <>{
 
+namespace target = hwlib::target; 
+
+class irReceiveControlClass : public rtos::task <>{
 private:
     uint16_t byte = 0x00;
     uint16_t byte_index = 32'768; //eerste bit links
@@ -69,6 +70,7 @@ private:
                                 }
                                 break;
                             }
+                            
                             if(hwlib::now_us()>=(connection_timeout+40000)){
                                 state = IDLE;
                                 break;             
@@ -104,16 +106,15 @@ private:
                     state = IDLE;
                     break;
                 }
-
             }
         }
     }
 
 public:
     irReceiveControlClass(RunGameClass & runGame, Registergame & regGame): 
-    rtos::task<>("irreceive"), 
-    runGame(runGame), 
-    regGame(regGame)
+        rtos::task<>("irreceive"), 
+        runGame(runGame), 
+        regGame(regGame)
     {}
 
 };
